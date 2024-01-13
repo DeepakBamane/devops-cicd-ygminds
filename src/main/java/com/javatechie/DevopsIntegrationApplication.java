41d028ec5346c779ed115bafa21141b3821ac9dc
DeepakBamane <102812335+DeepakBamane@users.noreply.github.com>
2024-01-13 19:26:36 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to Young Minds";
+		return "This is my Second Project";
 	}
 
 	public static void main(String[] args) {
