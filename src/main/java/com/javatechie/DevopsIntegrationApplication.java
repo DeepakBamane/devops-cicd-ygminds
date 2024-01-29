5a8c22073eed818f5728871ba0c22542cb501467
DeepakBamane <102812335+DeepakBamane@users.noreply.github.com>
2024-01-29 16:05:21 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "This is my Second Project";
+		return "This project is for creating EKS Cluster using terraform";
 	}
 
 	public static void main(String[] args) {
